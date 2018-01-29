8e2a8622ee9235eb5778de63686f44ceb2c7c368
Nicholas Kajoh <kajohterna@gmail.com>
2018-01-29 18:05:14 +0100

    add Heroku deploy configs

--- reactdjango/settings.py
+++ reactdjango/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'build/static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
