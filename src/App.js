f87f365105fd24adc1304db1a3ff4fe0b6f277eb
Nicholas Kajoh <kajohterna@gmail.com>
2018-01-30 05:31:10 +0100

    edit Create React App's default index page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import reactLogo from './logo-react.svg';
+import djangoLogo from './logo-django.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +8,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={reactLogo} className="App-logo" alt="React logo" />
+          <img src={djangoLogo} className="App-logo" alt="Django logo" />
+          <h1 className="App-title">Welcome to the React-Django app</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          I'm a blank canvas waiting for a coder's touch!
         </p>
       </div>
     );
